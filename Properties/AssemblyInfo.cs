03053801f16103bd76fd473d1bb62cced53219d9
TimKroket <nekkathecat@gmail.com>
2015-04-07 15:03:15 +0200

    x

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jinx_OneKeyToWin")]
+[assembly: AssemblyTitle("ForkedJinx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jinx_OneKeyToWin")]
+[assembly: AssemblyProduct("ForkedJinx")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.9.6.0")]
-[assembly: AssemblyFileVersion("2.9.6.0")]
+[assembly: AssemblyVersion("2.9.6.1")]
+[assembly: AssemblyFileVersion("2.9.6.1")]
